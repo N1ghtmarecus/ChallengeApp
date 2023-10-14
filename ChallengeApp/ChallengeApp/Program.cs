1ad1e700e519f38daea5fae3ea5f15cc7a8b3e1d
N1ghtmarecus <maciej.lubaszka.88@gmail.com>
2023-10-14 09:24:58 +0200

    Day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name    = "Ewa";
+var gender  = "female";
+var age     = 33;
+
+if (age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+}
+else if (age < 18 && gender != "female")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
+else
+{
+    Console.WriteLine("Przykro nam, nie mamy takiej osoby w bazie.");
+}
