8836c2d4c642609888259866f581b81e949cbe7a
N1ghtmarecus <maciej.lubaszka.88@gmail.com>
2023-10-20 00:03:56 +0200

    Day 7

---
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,21 @@
+namespace ChallengeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenEmployeeGetScore_ThenCheckCorrectResult()
+        {
+            // arrange
+            var employee = new Employee("Piotr", "Kleks", 41);
+            employee.AddScore(5);
+            employee.AddScore(6);
+            employee.AddScore(-15);
+
+            // act
+            var result = employee.Result;
+
+            // assert
+            Assert.AreEqual(-4, result);
+        }
+    }
+}
