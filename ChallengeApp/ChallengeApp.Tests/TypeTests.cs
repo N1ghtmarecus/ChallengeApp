b92a878b03d4dbf982341b9ee1468c0e4eb72ae0
N1ghtmarecus <maciej.lubaszka.88@gmail.com>
2023-10-20 16:42:12 +0200

    Day 8

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,49 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void WhenComparedTwoIntNumber_ShouldBeNotEqual()
+        {
+            // arrange
+            int number1 = 1;
+            int number2 = 2;
+            // act
+            // assert
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void WhenComparedTwoDoubleNumber_ShouldBeEqual()
+        {
+            // arrange
+            double number1 = 3.14;
+            double number2 = 3.14;
+            // act
+            // assert
+            Assert.AreEqual(number1, number2);
+        }
+
+        [Test]
+        public void WhenComparedTwoStrings_ShouldBeTheSame()
+        {
+            // arrange
+            string name1 = "Katarzyna";
+            string name2 = "Katarzyna";
+            // act
+            // assert
+            Assert.AreEqual(name1, name2);
+        }
+
+        [Test]
+        public void WhenComparedTwoObjects_ShouldNotBeTheSame()
+        {
+            // arrange
+            var employee1 = new Employee("Aleksandra", "Kowalska", 23);
+            var employee2 = new Employee("Aleksandra", "Kowalska", 23);
+            // act
+            // assert
+            Assert.AreNotEqual(employee1, employee2);
+        }
+    }
+}
